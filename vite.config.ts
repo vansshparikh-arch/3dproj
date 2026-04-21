8fc457965ceb5c8202faed7560193674c336f6d3
Vanssh Parikh <vanosski@Vansshs-MacBook-Air.local>
2026-04-21 16:24:35 +0530

    fix(deps): install @netlify/vite-plugin-tanstack-start and fix import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { netlify } from '@netlify/vite-plugin-tanstack-start'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
